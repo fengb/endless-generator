1d62ae5a69f30dc26a7549aa6b942d90957dd8ae
fengb <contact@fengb.info>
2015-05-26 21:28:29 -0500

    Added smarter test against es5/es6

--- test/endless-generator.js
+++ test/endless-generator.js
@@ -1,21 +1,40 @@
 var expect = require('./support').expect
 
-var endlessGenerator = require('..')
+
+function examplesFor(name, path, options){
+  options = options || {}
+
+  try {
+    var endlessGenerator = require(path)
+  } catch(err){
+    if(err instanceof SyntaxError && options.passOnSyntaxError){
+      return
+    } else {
+      throw err
+    }
+  }
+
+  describe(name, function(){
+    it('yields the passed-in value continuously', function(){
+      var gen = endlessGenerator('foo')
+      for(var i = 0; i < 1000; i++){
+        expect(gen).to.yield('foo')
+      }
+    })
+
+    it('yields original object', function(){
+      var obj = [{}]
+      var gen = endlessGenerator(obj)
+      expect(gen).to.yield(obj)
+      expect(gen).to.yield(obj)
+      expect(gen).to.yield(obj)
+      expect(gen).to.yield(obj)
+    })
+  })
+}
 
 describe('endless-generator', function(){
-  it('yields the passed-in value continuously', function(){
-    var gen = endlessGenerator('foo')
-    for(var i = 0; i < 1000; i++){
-      expect(gen).to.yield('foo')
-    }
-  })
-
-  it('yields original object', function(){
-    var obj = [{}]
-    var gen = endlessGenerator(obj)
-    expect(gen).to.yield(obj)
-    expect(gen).to.yield(obj)
-    expect(gen).to.yield(obj)
-    expect(gen).to.yield(obj)
-  })
+  examplesFor('index', '../index.js')
+  examplesFor('es5', '../endless-generator.js')
+  examplesFor('es6', '../endless-generator.es6', { passOnSyntaxError: true })
 })
